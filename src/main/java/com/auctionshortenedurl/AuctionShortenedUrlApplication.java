f3f9c448dee8f0674caee3d27ec140ea79d2046b
Muaz <gaballah.moaaz@gmail.com>
2021-09-22 08:30:33 +0300

    Testlerden vazgeçtim

--- src/main/java/com/auctionshortenedurl/AuctionShortenedUrlApplication.java
+++ src/main/java/com/auctionshortenedurl/AuctionShortenedUrlApplication.java
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 // eğer tamamen kaldırırsan test çalışmaz (ama requestleri alır)
 // eğer sadece repository koyarsan test çalışır ama diğer sınıfları kapattığı için request ler almaz
 // birden fazla sınıflara taraması sağlarsan belki olur.
-@ComponentScan(value = {"com.auctionshortenedurl.controller", "com.auctionshortenedurl.model", "com.auctionshortenedurl.repository"}) // this added to cover "Consider defining a bean of type ..."
+
+// test lerden vazgectiiim
+@ComponentScan(value = {"com.auctionshortenedurl.repository"}) // this added to cover "Consider defining a bean of type ..."
 @SpringBootApplication
 public class AuctionShortenedUrlApplication {
 
